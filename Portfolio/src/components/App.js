3f4fcc60bda16edbea4c1405f4967bd5fd1d95f4
Scott Campbell <cdn.zhredder@gmail.com>
2020-07-16 20:17:25 -0400

    Create route for partial feature list

--- Portfolio/src/components/App.js
+++ Portfolio/src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import Home from "./Home";
+import FeatureList from "./FeatureList";
+import store from "../stores/Store";
 
 export default class App extends Component {
     render() {
+        const fullList = <FeatureList heading={true} features={store}/>
+        const partialList = <FeatureList heading={false} features={store.slice(1, 3)}/>
+
         return (
             <div>
-                <Route exact path='/' render={(props) => <Home/>}/>
+                <Route exact path='/' render={(props) => fullList}/>
+                <Route exact path='/partial' render={(props) => partialList}/>
             </div>
         );
     }
--- Portfolio/src/components/FeatureList.js
+++ Portfolio/src/components/FeatureList.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from "react-bootstrap";
-
 import Feature from "./Feature";
 import Heading from "./Heading";
 
-import features from "../stores/Store";
-
-export default class Marketing extends Component {
+export default class FeatureList extends Component {
     render() {
+        const { heading, features } = this.props;
+
         const components = features.map((feature, index) => {
             return <Feature id={index} feature={feature}/>   
         });
@@ -16,8 +15,8 @@ export default class Marketing extends Component {
             <Grid style={{marginBottom: "3em"}}>
                 <Row>
                     <Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3}>
-                        <Heading/>
-                        {components}
+                        { heading ? <Heading/> : null }
+                        { components }
                     </Col>
                 </Row>
             </Grid>
