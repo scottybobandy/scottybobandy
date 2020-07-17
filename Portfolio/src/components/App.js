4fbe16e78c642d553c1026310392d9204560ffe9
Scott Campbell <cdn.zhredder@gmail.com>
2020-07-16 21:31:25 -0400

    Remove partial FeatureList

--- Portfolio/src/components/App.js
+++ Portfolio/src/components/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import FeatureList from "./FeatureList";
-import store from "../stores/Store";
 
 export default class App extends Component {
     render() {
-        const fullList = <FeatureList heading={true} features={store}/>
-        const partialList = <FeatureList heading={false} features={store.slice(1, 3)}/>
-
         return (
             <div>
-                <Route exact path='/' render={(props) => fullList}/>
-                <Route exact path='/partial' render={(props) => partialList}/>
+                <Route exact path='/' render={(props) => <FeatureList/>}/>
             </div>
         );
     }
--- Portfolio/src/components/FeatureList.js
+++ Portfolio/src/components/FeatureList.js
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import { Grid, Row, Col } from "react-bootstrap";
 import Feature from "./Feature";
 import Heading from "./Heading";
+import FeatureStore from "../stores/FeatureStore";
 
 export default class FeatureList extends Component {
     render() {
-        const { heading, features } = this.props;
-
-        const components = features.map((feature, index) => {
+        const features = FeatureStore.map((feature, index) => {
             return <Feature id={index} feature={feature}/>   
         });
 
@@ -15,8 +14,8 @@ export default class FeatureList extends Component {
             <Grid style={{marginBottom: "3em"}}>
                 <Row>
                     <Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3}>
-                        { heading ? <Heading/> : null }
-                        { components }
+                        <Heading/>
+                        { features }
                     </Col>
                 </Row>
             </Grid>
