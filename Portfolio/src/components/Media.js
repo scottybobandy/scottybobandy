d83fae2dbe5e81047d61d3eac90e005439ad6b62
Scott Campbell <cdn.zhredder@gmail.com>
2020-07-18 18:29:26 -0400

    Pull in latest Bootstrap

--- Portfolio/src/components/Media.js
+++ Portfolio/src/components/Media.js
@@ -6,13 +6,13 @@ export default function Media(props) {
 
     return (
         <div className="media" style={{ verticalAlign: "middle", marginTop: "1em" }}>
-            <div className="media-left">
+            <div className="media-left" style={{ paddingRight: "10px" }}>
                 <Image src={ image } className="media-object" style={{ height: "42px" }}/>
             </div>
             <div className="media-body">
-                <h4 className="media-heading" style={{ margin: "0" }}>
+                <h5 className="media-heading" style={{ margin: "0" }}>
                     { title }
-                </h4>
+                </h5>
                 <p style={{ margin: "0", color: "rgba(47,79,79,0.75)" }}>
                     { subtitle }
                 </p>
