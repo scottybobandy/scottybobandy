67f3eb824a6ef75a8bc75c43bb1b774c59961b8b
Campbell-Scott <scott@scottcampbell.dev>
2022-01-30 18:04:24 -0500

    Content updates, 01-30-2022

--- portfolio/src/pages/home/stores/HomeDataStore.js
+++ portfolio/src/pages/home/stores/HomeDataStore.js
@@ -1,30 +1,30 @@
 export default [
     {
-        "role": "Senior iOS Developer",
+        "role": "Senior iOS Engineer",
         "place": "Shopify",
         "timeframe": "February 2021 @ ",
         "url": "https://www.shopify.com"
     },
     {
-        "role": "Co-creator & Developer",
+        "role": "Co-creator",
         "place": "Gauntlet",
         "timeframe": "February 2019 @ ",
         "url": "https://gauntletapp.com"
     },
     {
-        "role": "iOS Developer",
+        "role": "iOS Engineer",
         "place": "Shopify",
         "timeframe": "July 2018 @ ",
         "url": "https://gauntletapp.com"
     },
     {
-        "role": "iOS Developer",
+        "role": "iOS Engineer",
         "place": "Vivametrica",
         "timeframe": "January 2018 @ ",
         "url": "https://www.vivametrica.com/"
     },
     {
-        "role": "Co-creator & Developer",
+        "role": "Co-creator",
         "place": "Dot Confirmed",
         "timeframe": "September 2016 @ ",
         "url": "http://dotconfirmed.com"
