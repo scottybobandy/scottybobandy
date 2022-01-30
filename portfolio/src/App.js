c3b9310b180e4ec1c176264f03cf967fb08e08c5
Campbell-Scott <scott@scottcampbell.dev>
2022-01-30 17:58:04 -0500

    Update portfolio, 01-30-2022

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Decks from "./pages/decks/components/Decks";
 import Home from "./pages/home/components/Home";
 
 export default function App() {
     return (
         <div>
             <Route exact path='/' component={ Home }/>
-            <Route exact path='/decks' component={ Decks }/>
         </div>
     );
 }
